dc4ca7970d00070e91cd5d8b402acbd7bf4c0181
Florent Benoit <Florent.Benoit@serli.com>
2014-08-11 14:29:01 +0200

    Fixed equals

--- core/src/main/java/com/codenvy/client/core/model/DefaultProjectReference.java
+++ core/src/main/java/com/codenvy/client/core/model/DefaultProjectReference.java
@@ -182,9 +182,6 @@ public class DefaultProjectReference implements ProjectReference {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
         if (obj == null) {
             return false;
         }
