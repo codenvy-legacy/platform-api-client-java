72ad0b8df563e2536902c14a4d80b8c626a3805e
Florent Benoit <Florent.Benoit@serli.com>
2014-10-15 16:06:33 +0200

    Add Jackson annotations

--- core/src/main/java/com/codenvy/client/core/model/DefaultProjectProblem.java
+++ core/src/main/java/com/codenvy/client/core/model/DefaultProjectProblem.java
@@ -12,24 +12,59 @@
 package com.codenvy.client.core.model;
 
 import com.codenvy.client.model.ProjectProblem;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 /**
  * @author Florent Benoit
  */
+@JsonInclude(NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DefaultProjectProblem implements ProjectProblem {
+
+    /**
+     * Error code.
+     */
+    private int code;
+
+    /**
+     * Error message.
+     */
+    private String message;
+
+    /**
+     * Build a new instance.
+     * @param code the given error code
+     * @param message the error message
+     */
+    @JsonCreator
+    public DefaultProjectProblem(
+            @JsonProperty("code") int code,
+            @JsonProperty("message") String message) {
+        this.code = code;
+        this.message = message;
+
+    }
+
     /**
      * @return error code
      */
     @Override
+    @JsonProperty("code")
     public int getCode() {
-        return 0;
+        return code;
     }
 
     /**
      * @return the problem's message
      */
     @Override
+    @JsonProperty("message")
     public String getMessage() {
-        return null;
+        return message;
     }
 }
